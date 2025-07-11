c3d637faaaa1c23052fd7560edf5c7ed9d98798c
moiseefsantos <moise.efsantos@gmail.com>
2025-07-10 21:40:21 -0400

    updated files

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -5,9 +5,29 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="/">GiftLink</a>
 
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
             <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    {/* Task 1: Add links to Home and Gifts below*/}
+                <ul className="navbar-nav ml-auto">
+
+                    {/* Task 1: Add links to Home and Gifts below */}
+                    <li className="nav-item">
+                        <a className="nav-link" href="/home.html">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/app">Gifts</a>
+                    </li>
+
                 </ul>
             </div>
         </nav>
